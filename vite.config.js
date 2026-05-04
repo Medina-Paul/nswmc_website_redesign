4302e36ff5ae9c8924363883eb40ba41ecaa806f
Paul Medina <paulmedina645@gmail.com>
2026-05-04 11:22:26 +0800

    fix node poly fills

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
